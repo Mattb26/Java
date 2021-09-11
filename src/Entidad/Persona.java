5b50ab29f4ff142c0cdc6ae1fe851c2e432fce90
bostero854 <bostero854@gmail.com>
2021-09-11 14:14:33 -0300

    Login

---
+++ src/Entidad/Persona.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Desarrollo
+ */
+public class Persona {
+    public int IdPersona;
+    public String Nombre ;
+    public String Apellido;
+    public String CorreoElectronico;
+    public Date FechaNacimiento;
+
+    public int getIdPersona() {
+        return IdPersona;
+    }
+
+    public void setIdPersona(int IdPersona) {
+        this.IdPersona = IdPersona;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return CorreoElectronico;
+    }
+
+    public void setCorreoElectronico(String CorreoElectronico) {
+        this.CorreoElectronico = CorreoElectronico;
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+}
--- src/Servicios/Api.java
+++ src/Servicios/Api.java
@@ -56,4 +56,42 @@ public class Api {
         
         return false;
     }
+    
+    public String Llamado(String jsonString, String uriString)
+    {
+        
+        try {
+        
+        System.out.println(jsonString);
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                .uri(URI.create(uriString))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
+                .header("Content-Type", "application/json")
+                .build();
+        
+          HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+         
+          System.out.println(response.statusCode());
+
+        // print response body
+        System.out.println(response.body());     
+        
+        if(response.statusCode()==200){
+            //Retorno el estado de la api
+            return  response.body();
+        }else{
+        
+            return "Error";
+        }
+        
+
+        
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return "";
+    }
 }
